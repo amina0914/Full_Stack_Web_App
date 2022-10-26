1bba1110c483716239f29870b0c1045a3fd04bf1
amina.turd <aminaturdalieva@gmail.com>
2022-10-26 12:16:18 -0400

    created the file for the io

---
+++ fileio/fileio.mjs
@@ -0,0 +1,29 @@
+/**
+ * This is a module that exports a function that reads a file information and returns it
+ * 
+ * */
+const fs = require("fs/promises");
+
+//Not sure if need to validate the file
+export async function validate (file){
+  try {
+    let stats = await fs.stat(file);
+    console.log("File is valid!");
+    return stats.isFile();
+  } catch (e) {
+    console.error("Error occured, file not valid: " + e);
+    throw e;
+  }
+}
+
+export async function read(file) {
+  try {
+    let data = await fs.readFile(file, "utf-8");
+    console.log("File read");
+    return JSON.parse(data).sort();
+  } catch (err) {
+    console.error(err.message);
+    throw e;
+  }
+}
+
