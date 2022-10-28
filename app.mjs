c96cea97f17480d198da604f59b2b943a27252ef
amina.turd <aminaturdalieva@gmail.com>
2022-10-28 15:58:25 -0400

    added second get method to the express route

--- app.mjs
+++ app.mjs
@@ -1,10 +1,25 @@
 import express from 'express';
+import {Singleton} from './controllers/controllerIO.mjs';
+import { fetchAPI } from './controllers/controllerFetch.mjs';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   if (req.query.name !== null ){
-    res.send({message: "Test " });
+    let single = new Singleton();
+    let data = await single.getInstance();
+    res.send({message: "Test " + JSON.stringify(data) });
+  } else {
+    res.status(404).send("404 not found");
+  }
+});
+
+app.get('/Symbol', async (req, res) => {
+  if (req.query.name !== null ){
+    // AAPL hardcoded, need to change to a regex of symbols 
+    let result = await fetchAPI('AAPL');
+    res.send({message: "Test " + JSON.stringify(result)});
   } else {
     res.status(404).send("404 not found");
   }
