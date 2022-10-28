cbfc9d63b0c420bb9994316f7e66b88ccbcf77fb
Jacky Tat <tatjacky123@gmail.com>
2022-10-28 18:18:57 -0400

    Button changes dom text

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,7 +2,31 @@
 
 document.addEventListener('DOMContentLoaded', setup)
 
-function setup(){
+const submitBtn = document.querySelector("button");
+submitBtn.addEventListener("click", fetch2);
 
+function setup() {
+  fetch1()
 }
 
+/**
+ * First Fetch to obtain the list of all symbols
+ */
+async function fetch1() {
+  // Fetch TODO
+}
+
+/**
+ * Second Fetch, executed only after pressing the submit button
+ * @param {event} e 
+ */
+async function fetch2(e) {
+  // Fetch TODO
+
+  let input = document.querySelector("#stock-choice").value;
+
+  let result = document.querySelector("#result");
+  result.textContent = `Current Price of ${input}: `;
+
+  e.preventDefault();
+}
