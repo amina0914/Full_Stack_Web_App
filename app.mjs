21c15b861af0a34b7b7475cd0a7862cd44e24890
Jacky Tat <tatjacky123@gmail.com>
2022-10-29 14:16:08 -0400

    res.json() instead of res.send() for data

--- app.mjs
+++ app.mjs
@@ -1,27 +1,38 @@
 import express from 'express';
-import {Singleton} from './controllers/controllerIO.mjs';
+import { Singleton } from './controllers/controllerIO.mjs';
 import { fetchAPI } from './controllers/controllerFetch.mjs';
 
 const app = express();
 const port = 3000;
 
+// This is the homepage, in other words, the /public/index.html
 app.get('/', async (req, res) => {
-  if (req.query.name !== null ){
+  if (req.query.name !== null) {
     let single = new Singleton();
     let data = await single.getInstance();
-    res.send({message: "Test " + JSON.stringify(data) });
+    res.send({ message: "Test " + JSON.stringify(data) });
   } else {
     res.status(404).send("404 not found");
   }
 });
 
+// This is the request to obtain data
 app.get('/Symbol', async (req, res) => {
-  if (req.query.name !== null ){
-    // AAPL hardcoded, need to change to a regex of symbols 
-    let result = await fetchAPI('AAPL');
-    res.send({message: "Test " + JSON.stringify(result)});
+  // If there are no query, return json list of all Symbols
+  if (req.query.name == null) {
+    let single = new Singleton();
+    let data = await single.getInstance();
+    res.json(data.symbolsArray);
   } else {
-    res.status(404).send("404 not found");
+    // Request to obtain stock information
+    let result = await fetchAPI(req.query.name);
+    // If Stock does not exist / current price = 0
+    if (result.c == 0) {
+      res.status(404).send("404 not found");
+    }
+    else {
+      res.json(result);
+    }
   }
 });
 
