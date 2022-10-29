7dac2bfba9a5726b5fdf9ec0140d024c9d5f27f4
Jacky Tat <tatjacky123@gmail.com>
2022-10-29 19:53:06 -0400

    connected fetch functionality with express

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,18 +2,28 @@
 
 document.addEventListener('DOMContentLoaded', setup)
 
-const submitBtn = document.querySelector("button");
-submitBtn.addEventListener("click", fetch2);
+const form = document.querySelector("form");
+form.addEventListener("submit", getStock);
+const symbolList = document.querySelector("#stock-list")
 
 function setup() {
-  fetch1()
+  getSymbols();
 }
 
 /**
  * First Fetch to obtain the list of all symbols of stocks
  */
-async function fetch1() {
-  // Fetch TODO
+async function getSymbols() {
+  let response = await fetch("http://localhost:3000/Symbol")
+  let content;
+  if (response.ok) {
+    content = await response.json();
+  }
+  content.forEach(element => {
+    let option = document.createElement("option");
+    option.value = element.symbol;
+    symbolList.appendChild(option);
+  })
 }
 
 /**
@@ -21,13 +31,29 @@ async function fetch1() {
  * Updates the DOM with the result of the Search query
  * @param {event} e 
  */
-async function fetch2(e) {
-  // Fetch TODO
+async function getStock(e) {
+  e.preventDefault();
 
+  // Fetch TODO
   let input = document.querySelector("#stock-choice").value;
 
-  let result = document.querySelector("#result");
-  result.textContent = `Current Price of ${input}: `;
+  let url = `http://localhost:3000/Search?name=${input}`
 
-  e.preventDefault();
+  let response = await fetch(url)
+  let content;
+  if (response.ok) {
+    content = await response.json();
+  }
+  let result = document.querySelector("#result");
+  
+  if (result.classList.contains("error")) {
+    result.classList.remove("error");
+  }
+
+  if (content.c == 0) {
+    result.textContent = `ERROR: ${input} does not exist`;
+    result.classList.add("error");
+  } else {
+    result.textContent = `Current Price of ${input} is: $${content.c}`;
+  }
 }
